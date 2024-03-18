fc8c3c125f227f2d706b784b9c40f283f0923b1c
Kirill.Smolyakov <smolyakov.kirusha@gmail.com>
2024-03-19 01:08:07 +0600

    add under control click listeners for main activity buttons

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val search = findViewById<Button>(R.id.search_btn)
+
+        val searchClickListener: View.OnClickListener = View.OnClickListener { Toast.makeText(this@MainActivity, "Нажали на кнопку поиска!", Toast.LENGTH_SHORT).show() }
+
+        search.setOnClickListener(searchClickListener)
+
+        val mediateka = findViewById<Button>(R.id.mediateka_btn)
+
+        mediateka.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку медиатеки!", Toast.LENGTH_SHORT).show()
+        }
+
+        val settings = findViewById<Button>(R.id.settings_btn)
+
+        settings.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на кнопку настроек!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
