93077bf4a5c1a6511dccbe015bf6983d5600d844
SmolyakovKirill <smolyakov.kirusha@gmail.com>
2024-07-04 01:19:27 +0600

    add under control supportButton, termsOfUseButton

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -23,5 +24,36 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(Intent.createChooser(shareData, "Share Link"))
         }
         shareButton.setOnClickListener(shareClickListener)
+
+        val supportButton = findViewById<ImageButton>(R.id.support_btn)
+
+        val supportClickListener: View.OnClickListener = View.OnClickListener {
+            val emailSend = "smolyakov.kirusha@gmail.com"
+            val emailSubject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val emailBody = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+            val intent = Intent(Intent.ACTION_SEND)
+
+            intent.putExtra(Intent.EXTRA_EMAIL, emailSend)
+            intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject)
+            intent.putExtra(Intent.EXTRA_TEXT, emailBody)
+
+            intent.type = "message/rfc822"
+
+            startActivity(Intent.createChooser(intent, "Email to developer"))
+        }
+        supportButton.setOnClickListener(supportClickListener)
+
+        val termsOfUse = findViewById<ImageButton>(R.id.term_of_use_btn)
+
+        val termsOfUseListener: View.OnClickListener = View.OnClickListener {
+            val url = "https://yandex.ru/legal/practicum_offer/"
+            val urlIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(url)
+            )
+            startActivity(urlIntent)
+        }
+        termsOfUse.setOnClickListener(termsOfUseListener)
     }
 }
